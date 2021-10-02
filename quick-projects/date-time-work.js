5303a3c9be201ab3fb3e52390a435edbe64dec1f
jonathan gober <Gober.jonathan@gmail.com>
2021-10-02 11:47:27 -0400

    final date stuff for now

--- quick-projects/date-time-work.js
+++ quick-projects/date-time-work.js
@@ -1,7 +1,16 @@
 var moment = require("moment"); // require
-var today = moment().format('MMMM Do YYYY, h:mm:ss a');
-var addDays = moment().add(30, 'days').calendar(); 
-var subDays = moment().subtract(10, 'days').calendar()  
+var today = moment().format("MMMM Do YYYY, h:mm:ss a");
+var addDays = moment().add(30, "days").calendar();
+var subDays = moment().subtract(10, "days").calendar();
+var all = moment()
+  .add(7, "days")
+  .subtract(1, "months")
+  .year(2009)
+  .hours(0)
+  .minutes(0)
+  .seconds(0);
+
 console.log(today);
 console.log(addDays);
 console.log(subDays);
+console.log(all);
